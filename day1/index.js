ae790ba9e321aadae04c3cc7f32b49c27f6f41ba
tkhadir <tkhadir.dev@gmail.com>
2020-10-31 10:24:56 +0100

    :rocket: solved

--- day1/index.js
+++ day1/index.js
@@ -37,9 +37,42 @@ let test = (testData) => {
 let testData = ['12', '14', '1969', '100756']
 test(testData)
 
-console.log('compute solution :------------')
-let solution = inputs.split('\n')
+console.log('part 1 : compute solution :------------')
+let s1 = inputs.split('\n')
                 .filter(i => i.length > 0)
                 .map(d => computeFuel(d))
                 .reduce((d1, d2) => d1 + d2, 0)
-console.log('final solution : ' + solution)
+console.log('final solution : ' + s1)
+
+
+console.log('------------------------------')
+
+let computeRealFuel = (data) => {
+    let result = 0
+    let needComputing = true
+    let tmp = data
+    while (needComputing) {
+        tmp = computeFuel(tmp)
+        result += (tmp > 0 ? tmp : 0)
+        needComputing = tmp > 0
+    }
+    return result
+}
+
+
+let test2 = (testData) => {
+    testData
+    .filter(t => t.length > 0)
+    .map(t => [t, computeRealFuel(t)])
+    .forEach(r => {
+        console.log('comput real fuel for ' + r[0] + ' fuel = ' + r[1])
+    })
+}
+test2(testData)
+
+console.log('part 2 : compute solution :-------------')
+let s2 = inputs.split('\n')
+            .filter(i => i.length > 0)
+            .map(d => computeRealFuel(d))
+            .reduce((d1, d2) => d1 + d2, 0)
+console.log('final solution : ' + s2)
