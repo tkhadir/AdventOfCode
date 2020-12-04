28c10fe924528eece2873db214576933e2345e15
tkhadir <tkhadir.dev@gmail.com>
2020-12-04 23:15:15 +0100

    :rocket: solve day 6

--- day6/index.js
+++ day6/index.js
@@ -1,5 +1,20 @@
 const fs = require('fs')
 
+class Planet {
+    constructor(k, v) {
+        this.key = k
+        this.inOrbitArround = v
+    }
+
+    get name() {
+        return this.key
+    }
+
+    get origine() {
+        return this.inOrbitArround
+    }
+}
+
 let getRange = (start, end) => {
     let stream = []
     for (let i=start; i<= end; i++) {
@@ -24,4 +39,27 @@ let cloneArr = (data) => {
     return clone
 }
 
+const input = readContent('./input.txt')
+
+let planets = []
+let links = []
+
+input.split('\r\n').forEach(i => {
+    let split = i.split('\)')
+    planets.push(new Planet(split[1], split[0]))
+})
+
+const addIndirectLink = (p) => {
+    if (p === undefined) return
+    let op = planets.find(op$ => op$.name === p.origine)
+    if (op && !links[op.name+'-'+p.origine]) links.push([op.name+'-'+p.origine])
+    addIndirectLink(op)
+}
+
+planets.forEach(p => {
+    if (!links[p.name+'-'+p.origine]) links.push([p.name+'-'+p.origine])
+    addIndirectLink(p)
+})
+
+console.log(links.length)
 
